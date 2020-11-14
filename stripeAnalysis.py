0ca9776777e9d9ec5b0084c539d90eb339973669
Nick Garcia <47847297+designi@users.noreply.github.com>
2020-11-13 22:22:47 -0800

    Update stripeAnalysis.py

--- stripeAnalysis.py
+++ stripeAnalysis.py
@@ -9,14 +9,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 import warnings
 
 #create predictions
-def calcAvg(df,cntry):
+def calc(df,cntry,plots=False):
     data = df[df.country==cntry].sort_values('date')
     data.set_index("date", inplace = True)
-    avg_data = data.groupby('date').mean()
-    return avg_data
+    results= data.groupby('date').sum()
+    if not plots:
+        return results
+    results.plot(title='Amount paid out to {} daily'.format(cntry))
+    if len(results) > 365/2:
+        results.rolling(14).mean().plot(title='Bi-weekly rolling average payouts to {}'.format(cntry))
+    
 
 def calcPrediction(df,countries,p=1,d=1,q=0,err_results=False):
     """
@@ -30,17 +36,17 @@ def calcPrediction(df,countries,p=1,d=1,q=0,err_results=False):
     for cntry in countries:
         print(' Working on {}'.format(cntry))
         # dataset
-        avg_data = calcAvg(df,cntry)
-        datapoints[cntry] = len(avg_data)
-        if len(avg_data) == 0: 
+        data = calc(df,cntry)
+        datapoints[cntry] = len(data)
+        if len(data) == 0: 
             prediction_results[cntry] = np.NaN
             print(' No data for {}. Investigate...'.format(cntry))
             continue
-        model = ARIMA(avg_data,order=(p,d,q))
+        model = ARIMA(data,order=(p,d,q))
         # fit ARIMA model
         model_fit = model.fit(disp=False)
         # make prediction 
-        yhat = model_fit.predict(start = len(avg_data), end=len(avg_data), typ='levels')
+        yhat = model_fit.predict(start = len(data), end=len(data), typ='levels')
         # cntry-prediction map
         prediction_results[cntry] = float(yhat)
         def examineErrors(model_fit):
@@ -56,6 +62,25 @@ def genPredictionGraph(prediction_results):
     if len(prediction_results) != 0:
         return plt.bar(x=prediction_results.keys(),height=prediction_results.values(),align='edge')
 
+def genIndustryPredictions(df,industries,plots=False):
+    prediction_results = {}
+    for ind in industries:
+        print(' Working on {}'.format(ind))
+        df_ind = df[df.industry==ind].sort_values('date')
+        df_ind.set_index("date", inplace = True)
+        df_ind.index = pd.to_datetime(df_ind.index)
+        tdv = df_ind['amount'].groupby('date').count()
+        model = SARIMAX(tdv, order=(1, 1, 1), seasonal_order=(1, 1, 1, 52),trend='t')
+        # fit SARIMA model
+        model_fit = model.fit(disp=False)
+        # make prediction 
+        yhat = model_fit.predict(start=len(tdv), end=len(tdv), typ='levels')
+        prediction_results[ind] = int(yhat)
+        if plots:
+            tdv.plot(title=ind)
+            tdv.diff()[1:].plot(title=ind+' lagged 1 period')
+    return prediction_results
+        
 if __name__=='__main__':
     Verbose = False
     print(' Loading csv files...')
@@ -77,15 +102,7 @@ if __name__=='__main__':
     # Reassign for predictions
     df = df_merge_pymnt_cntry
     df.dropna(inplace=True)
-    
-    # Aggregate total payouts by date by country
-    payout_by_date_cntry = df.groupby(['date','country']).sum()
-    payout_by_date_cntry.sort_values(['date','country'])
-    
-    # Aggregate total payouts by country by date 
-    payout_by_cntry_date = df.groupby(['country','date']).sum()
-    payout_by_cntry_date.sort_values(['country','date'])
-    
+
     # Drop count column
     df.drop(labels='count',inplace=True,axis=1)
     
@@ -95,14 +112,25 @@ if __name__=='__main__':
     print(' \n Finished calculating predictions...\n\n {} \n\n '.format(r))
     
     # Industry analysis
-    df_merge_ind_cntry = pd.merge(df_payouts,df_industries,left_on='recipient_id',right_on='merchant_id', how='inner')
-    df_merge_ind_cntry.sort_values(['industry','date'],inplace=True)
-    ind = sorted(list(df_merge_ind_cntry.industry.unique()))
+    df_merge_ind_payout = pd.merge(df_payouts,df_industries,left_on='recipient_id',right_on='merchant_id', how='inner')
     
+    # Drop count column
+    df_merge_ind_payout.drop(labels='count',inplace=True,axis=1)
+    
+    # Sort the df on ind and dt
+    df_merge_ind_payout.sort_values(['industry','date'],inplace=True)
+    industries = sorted(list(df_merge_ind_payout.industry.unique()))
+
     # Map parameters
-    paramdct = {'Education':15,'Hotels, Restaurants & Leisure':5,'Food & Beverage':40}
+    paramdct = {'Education':15,'Travel & Hospitality':5,'Food & Beverage':40}
+    industries = list(paramdct.keys())
     
-    df_merge_ind_cntry.groupby(['date','industry'])['amount'].sum()
-    
-    
-    
+    #generate industry predictions
+    res = genIndustryPredictions(df_merge_ind_payout,industries)
+    print(' \n Finished calculating predictions...\n\n {} \n\n '.format(res))
+
+
+
+
+
+
